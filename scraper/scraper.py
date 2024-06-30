84b6d20efdd629f2fd06dde9c7bb9910a0f24002
Vitto-Mazeto <vittopicma@gmail.com>
2024-06-30 18:00:29 -0300

    feat: initial scrapper

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,1 +1,47 @@
-print('test')
+import requests
+from bs4 import BeautifulSoup
+import re
+
+def fetch_contacts(url):
+    try:
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+        }
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+
+        soup = BeautifulSoup(response.text, 'html.parser')
+        
+        email_pattern = r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}'
+        cellphone_pattern = r'\(?\b[1-9]{2}\)?[-.\s]?[9]{0,1}\s?[0-9]{4}[-.\s]?[0-9]{4}\b'
+
+        emails = re.findall(email_pattern, soup.get_text())
+        cellphones = re.findall(cellphone_pattern, soup.get_text())
+
+        # Remover duplicatas
+        emails = list(set(emails))
+        cellphones = list(set(cellphones))
+
+        return {
+            'site': url,
+            'celular': cellphones,
+            'email': emails,
+        }
+    except requests.RequestException as e:
+        print(f"Erro ao acessar {url}: {e}")
+        return {
+            'site': url,
+            'celular': [],
+            'email': []
+        }
+
+if __name__ == "__main__":
+    urls = ['https://cortezeimoveis.com.br/', 'https://ribeiroimoveis.com.br/', 'https://www.maiaimoveissjp.com.br/', 'https://venturiimoveis.com.br/', 'https://www.roccoimoveis.com.br/', 'https://alegriaimobiliaria.com.br/', 'https://www.suelifernandes.com/']
+    contacts_list = []
+
+    for url in urls:
+        contacts = fetch_contacts(url)
+        contacts_list.append(contacts)
+    
+    for contacts in contacts_list:
+        print(contacts)
