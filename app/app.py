f1f4d34af855847cdabd9162b0ca8a3058367015
Vitto-Mazeto <vittopicma@gmail.com>
2024-07-07 23:26:30 -0300

    feat: app inicial and streamlit

--- app/app.py
+++ app/app.py
@@ -1,15 +1,15 @@
 import sqlite3
 import os
 import random
+import streamlit as st
+import pandas as pd
 
 # Função mockada para google_search
 def mock_google_search(query, num_results):
-    # Retorna uma lista de URLs mockadas
     return [f"https://example.com/{i}" for i in range(num_results)]
 
 # Função mockada para fetch_contacts
 def mock_fetch_contacts(url):
-    # Retorna um dicionário com contatos mockados
     return {
         'site': url,
         'celular': [f"({random.randint(10, 99)}) {random.randint(90000, 99999)}-{random.randint(1000, 9999)}"],
@@ -45,28 +45,54 @@ def save_contacts_to_db(db_path, contacts_list, city):
     conn.commit()
     conn.close()
 
-if __name__ == "__main__":
-    city = "Santiago"
-    query = f"terrenos imobiliaria {city}"
-    num_results = 50
+def fetch_all_contacts(db_path):
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM contacts")
+    rows = cursor.fetchall()
+    conn.close()
+    return rows
 
-    # Criar a pasta data se não existir
+def main():
+    # Cria a pasta data se não existir
     if not os.path.exists('data'):
         os.makedirs('data')
 
     db_path = os.path.join('data', 'contacts.sqlite')
 
-    # Criar o banco de dados se não existir
+    # Cria o banco de dados se não existir
     create_database(db_path)
 
-    urls = mock_google_search(query, num_results)
-    contacts_list = []
+    st.title("RPA Contatos de Imobiliárias")
 
-    for url in urls:
-        contacts = mock_fetch_contacts(url)
-        contacts_list.append(contacts)
+    tab1, tab2 = st.tabs(["Buscar Contatos", "Tabela de busca"])
 
-    # Salvar a contacts_list no banco de dados
-    save_contacts_to_db(db_path, contacts_list, city)
+    with tab1:
+        st.header("Buscar Contatos")
+        city = st.text_input("Localização", value="São José dos Campos")
+        num_results = st.number_input("Número de Resultados", min_value=1, max_value=1000, value=50)
 
-    print("Contacts saved to", db_path)
+        if st.button("Buscar"):
+            query = f"terrenos imobiliaria {city}"
+            urls = mock_google_search(query, num_results)
+            contacts_list = []
+
+            for url in urls:
+                contacts = mock_fetch_contacts(url)
+                contacts_list.append(contacts)
+
+            save_contacts_to_db(db_path, contacts_list, city)
+            st.success(f"Contatos salvos para {city}")
+
+    with tab2:
+        st.header("Contatos")
+        contacts = fetch_all_contacts(db_path)
+
+        if contacts:
+            df = pd.DataFrame(contacts, columns=["ID", "Site", "Celular", "Email", "Cidade"])
+            st.dataframe(df)
+        else:
+            st.write("Sem contatos até o momento.")
+
+if __name__ == "__main__":
+    main()
