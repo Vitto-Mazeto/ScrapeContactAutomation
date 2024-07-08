9db6842a95c8e66c9c5b0a53326a65afd301bb56
Vitto-Mazeto <vittopicma@gmail.com>
2024-07-08 15:45:44 -0300

    refactor: early_return on contatcts_table

--- app/contacts_table.py
+++ app/contacts_table.py
@@ -10,25 +10,27 @@ def run():
     db_path = os.path.join('data', 'contacts.sqlite')
     contacts = fetch_all_contacts(db_path)
 
-    if contacts:
-        df = pd.DataFrame(contacts, columns=["ID", "Site", "Celular", "Email", "Cidade", "Mensagens Enviadas"])
-        df['Selecionar'] = False
+    # Se não houver contatos, exibe mensagem e retorna
+    if not contacts:
+        st.write("Nenhum contato encontrado")
+        return
 
-        config = {
-            "Selecionar": st.column_config.CheckboxColumn(
-                "Selecionar",
-                help="Selecione as linhas para enviar mensagem",
-                default=False
-            )
-        }
+    df = pd.DataFrame(contacts, columns=["ID", "Site", "Celular", "Email", "Cidade", "Mensagens Enviadas"])
+    df['Selecionar'] = False
 
-        edited_df = st.data_editor(df, column_config=config, hide_index=True)
+    config = {
+        "Selecionar": st.column_config.CheckboxColumn(
+            "Selecionar",
+            help="Selecione as linhas para enviar mensagem",
+            default=False
+        )
+    }
 
-        if st.button("Disparar Mensagem"):
-            selected_contacts = edited_df[edited_df['Selecionar']].to_dict(orient='records')
-            for contact in selected_contacts:
-                st.write(f"Enviando mensagem para {contact['Email']} e {contact['Celular']}")
-                update_message_count(db_path, contact['Site'])
-                df.loc[df['Email'] == contact['Email'], 'Mensagens Enviadas'] += 1
-    else:
-        st.write("Sem contatos até o momento.")
+    edited_df = st.data_editor(df, column_config=config, hide_index=True)
+
+    if st.button("Disparar Mensagem"):
+        selected_contacts = edited_df[edited_df['Selecionar']].to_dict(orient='records')
+        for contact in selected_contacts:
+            st.write(f"Enviando mensagem para {contact['Email']} e {contact['Celular']}")
+            update_message_count(db_path, contact['Site'])
+            df.loc[df['Email'] == contact['Email'], 'Mensagens Enviadas'] += 1
